aa25081466ed4de8fc9a904ee2712aa54cdf5d0f
NabokaEA <nabokae@yandex.ru>
2022-09-12 17:47:23 +0300

    Initial commit

---
+++ src/main/java/ru/nabokae/PP_3_1_1_1/Pp3111Application.java
@@ -0,0 +1,13 @@
+package ru.nabokae.PP_3_1_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp3111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp3111Application.class, args);
+	}
+
+}
---
+++ src/main/java/ru/nabokae/PP_3_1_1_1/userdata/UserRepository.java
@@ -0,0 +1,6 @@
+package ru.nabokae.PP_3_1_1_1.userdata;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
